aa3b2f8c9ab49110261ac8bd6073c5bb78d54e13
Magda Toczek <magdalena.toczek@gmail.com>
2020-09-12 18:18:58 +0200

    simple bar chart

---
+++ Drawing/View/SimpleBar.swift
@@ -0,0 +1,98 @@
+//
+//  SimpleBar.swift
+//  Drawing
+//
+//  Created by Magdalena Toczek on 12/09/2020.
+//  Copyright © 2020 Popeq Apps. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SimpleBar: UIView {
+
+    
+//    let data: [String: CGFloat] = [
+//
+//        "Mon" : 9,
+//        "Tue" : 5,
+//        "Wen" : 3,
+//        "Thu" : 15,
+//        "Fri" : 11,
+//    ]
+//
+    
+    let dataDays: [String] = [ "Mon", "Tue", "Wen", "Thu", "Fri" ]
+    let dataValues: [CGFloat] = [9,5,3,15,11]
+    
+
+    override func draw(_ rect: CGRect) {
+        
+        let ctx = UIGraphicsGetCurrentContext()
+        
+        let howManyItems: CGFloat = CGFloat(dataDays.count)
+        let maxValue : CGFloat = dataValues.max()!
+        
+        let viewWidth = bounds.width
+        let viewHeight = bounds.height
+        let padding : CGFloat = 30
+    
+        
+        let viewHeightMinusPadding = viewHeight - (2 * padding)
+        let viewWidthMinusPading = viewWidth - (2 * padding)
+        
+        
+        let unitForHeight = viewHeightMinusPadding/(maxValue)
+        
+        
+        
+        
+        
+        var barStartWidth : CGFloat = (viewWidth - viewWidthMinusPading)/2
+        var lineStartPosition : CGFloat = (viewWidth - viewWidthMinusPading)/2
+        var lineEndPosition : CGFloat = lineStartPosition + (maxValue * unitForHeight)
+       
+        //lines
+        for x in 0...15 {
+             ctx?.saveGState()
+            let path = UIBezierPath(rect: CGRect(x: barStartWidth, y: lineStartPosition, width: viewWidthMinusPading, height: 1))
+            lineStartPosition = lineStartPosition + unitForHeight
+            #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1).setFill()
+            path.fill()
+            ctx?.restoreGState()
+        }
+        
+        
+        
+        
+        
+        let columnWidth: CGFloat = 20
+        let paddingForColumns = (viewWidthMinusPading - ((howManyItems + 2) * columnWidth)) / howManyItems
+        print(paddingForColumns)
+        var startPosition = barStartWidth + paddingForColumns
+    
+        
+        for value in dataValues {
+           
+            ctx?.saveGState()
+            let heightColumn = -(value * unitForHeight)
+            
+            let path = UIBezierPath(rect: CGRect(x: startPosition, y: lineEndPosition, width: columnWidth, height: heightColumn))
+            
+            startPosition = startPosition + columnWidth + paddingForColumns
+            
+            #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1).setFill()
+            path.fill()
+            ctx?.restoreGState()
+            
+        }
+        
+        
+        
+        
+        
+ 
+    }
+ 
+
+}
