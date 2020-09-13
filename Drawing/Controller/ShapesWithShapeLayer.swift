a5a0478736450eb0eafc37a7da3c2ce3c8d8fe6a
Magda Toczek <magdalena.toczek@gmail.com>
2020-09-13 10:49:50 +0200

    cashapelayer properties -  lineJoin, dashPattern, lineCap

--- Drawing/Controller/ShapesWithShapeLayer.swift
+++ Drawing/Controller/ShapesWithShapeLayer.swift
@@ -31,14 +31,20 @@ class ShapesWithShapeLayer: UIViewController {
         
         let combinePath = UIBezierPath()
         
-        let circle1  = UIBezierPath(ovalIn: CGRect(x: 250, y: 120, width: 60, height: 60))
-        let circle2 = UIBezierPath(ovalIn: CGRect(x: 250, y: 160, width: 40, height: 40))
+        let circle1  = UIBezierPath(ovalIn: CGRect(x: 120, y: 120, width: 60, height: 60))
+        let circle2 = UIBezierPath(ovalIn: CGRect(x: 120, y: 160, width: 40, height: 40))
         
         combinePath.append(circle1)
         combinePath.append(circle2)
         
+        //circle position relative to frame - frame is needed o avoid problems
+        combineShape.frame = CGRect(x: 120, y: 0, width: 240, height: 240)
+        
         combineShape.path = combinePath.cgPath
         combineShape.strokeColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        combineShape.fillColor = nil
+        combineShape.lineDashPattern = [4, 3]
+        //combineShape.lineDashPhase = 10  //offset from start
         combineShape.lineWidth = 3.0
         shapesView.layer.addSublayer(combineShape)
         
@@ -59,6 +65,9 @@ class ShapesWithShapeLayer: UIViewController {
     
     func createSquare(){
         squareLayer.fillColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        squareLayer.strokeColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        squareLayer.lineWidth = 10.0
+        squareLayer.lineJoin = CAShapeLayerLineJoin.bevel
         shapesView.layer.addSublayer(squareLayer)
         
     }
@@ -66,6 +75,9 @@ class ShapesWithShapeLayer: UIViewController {
     func createCircle(){
         circleLayer.fillColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         circleLayer.strokeColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        circleLayer.lineJoin = CAShapeLayerLineJoin.round
+        circleLayer.strokeEnd = 0.7   //between 0-1
+        circleLayer.lineCap = CAShapeLayerLineCap.round
         circleLayer.lineWidth = 4.0
         shapesView.layer.addSublayer(circleLayer)
     }
